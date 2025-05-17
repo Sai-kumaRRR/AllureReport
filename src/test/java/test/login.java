8477b9d03f64be198fe440a7e74956e647a0b9b3
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-05-17 13:25:01 +0530

    17th May 2025

--- src/test/java/test/login.java
+++ src/test/java/test/login.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+package test;
+
+
+import org.testng.annotations.Test;
 
 public class login {
+
+    @Test
+    public void login1() {
+
+        System.out.println("Register Successfully 1");
+    }
+
+    @Test
+    public void login2() {
+        System.out.println("Register Successfully 2");
+    }
+
+
 }
